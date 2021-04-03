1a3bd8b2be9435eeb6fe62d5d840643927432fa3
Marcel Roig <marcelrm11@gmail.com>
2021-04-03 21:39:20 +0200

    VueJS

---
+++ M10. VueJS/5 Comunicacio/proyecto/src/components/Pare.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Component Pare</h3>
+        <Fill
+        v-for="item in items"
+        :key="item.index">
+        {{item}}
+        </Fill>
+    </div>
+</template>
+
+<script>
+import Fill from './Fill'
+
+export default {
+    name: 'Pare',
+    components: {
+        Fill
+    },
+    data() {
+        return {
+            items: [
+                'item 1',
+                'item 2',
+                'item 3'
+            ]
+        }
+    },
+}
+</script>
